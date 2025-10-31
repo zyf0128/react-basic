c400f1785695d6961de3e48e71d182c1b79f0e61
zhuyin <zhuyin_dsw@163.com>
2025-10-31 21:58:31 +0800

    简单的条件渲染

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 const count = 100;
 
-const list = [
-    {id: "01", name: "John", age: 12},
-    {id: "02", name: "Jack", age: 32},
-    {id: "03", name: "Mary", age: 33}
-]
+
+const isLogin = false
 
 // 项目的根组件
 function App() {
     return (
-        <div className="App">
-            {/*渲染列表*/}
-            {/*{map 循环哪个结构 return 哪个结构}*/}
-            {/*    注意事项： 加上一个独一无二的key, 字符串或者 number id*/}
-            {/*    用来提升 列表的更新性能*/}
-            <ul>
-                {
-                    list.map(
-                        item =>
-                    <li key={item.id}> {item.name}的年龄是： {item.age} </li>
-                    )
-                }
-            </ul>
+
+        <div>
+            {/* 条件渲染 */}
+            {/* 逻辑与 && */}
+            {isLogin && <span>Login</span>}
+
+            {/* 三元运算 */}
+            {isLogin ? <span>login</span> : <span> not login</span>}
         </div>
     );
 }
