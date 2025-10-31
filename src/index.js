3a616a295a04c8780772a11fa317e52e6fdd1eb0
zhuyin <zhuyin_dsw@163.com>
2025-10-31 20:04:55 +0800

    1. 使用cra创建了初始项目 2. 删除了无用文件和代码 3. 在App.js和index.js中添加了注释说明代码作用

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// react 必要的两个核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+//导入项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 把根组件渲染到 id 为 root 的 DOM 节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
+
