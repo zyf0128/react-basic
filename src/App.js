22a3e115c3be1e645441b61431a6f216553cbeb6
zhuyin <zhuyin_dsw@163.com>
2025-10-31 22:51:30 +0800

    复杂的条件渲染 if else

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 const count = 100;
 
+// 0,1,3
+var type = 0
 
-const isLogin = false
+// 实现一个核心的渲染逻辑
+
+function renderContent() {
+    {/* 通过if语句来实现复杂的渲染逻辑 */ }
+    if (type === 0) {
+        return <h1>Type is Zero</h1>;
+    } else if (type === 1) {
+        return <h2>Type is One</h2>;
+    } else {
+        return <h3>Type is Other</h3>;
+    }
+}
+
 
 // 项目的根组件
 function App() {
     return (
-
         <div>
-            {/* 条件渲染 */}
-            {/* 逻辑与 && */}
-            {isLogin && <span>Login</span>}
-
-            {/* 三元运算 */}
-            {isLogin ? <span>login</span> : <span> not login</span>}
+            {/* 通过调用函数来渲染 */}
+            {renderContent()} <br />
         </div>
     );
 }
