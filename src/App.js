5e10e545c489a6d63e34d00b768f12b526f8cd1a
zhuyin <zhuyin_dsw@163.com>
2025-10-31 23:51:54 +0800

    事件绑定

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-const count = 100;
 
-// 0,1,3
-var type = 0
-
-// 实现一个核心的渲染逻辑
-
-function renderContent() {
-    {/* 通过if语句来实现复杂的渲染逻辑 */ }
-    if (type === 0) {
-        return <h1>Type is Zero</h1>;
-    } else if (type === 1) {
-        return <h2>Type is One</h2>;
-    } else {
-        return <h3>Type is Other</h3>;
-    }
-}
-
+{/* 事件的绑定 */ }
 
 // 项目的根组件
 function App() {
+    //  基础绑定
+    const handleClick01 = () => {
+        console.log('按钮被点击了1')
+    };
+
+    //事件参数e
+    const handleClick02 = (e) => {
+        console.log('按钮被点击了2', e)
+    };
+
+    // 自定义参数name
+    const handleClick03 = (name) => {
+        console.log('按钮被点击了3', name)
+    }
+
+    // 自定义参数name + 事件参数e
+    const handleClick04 = (name, e) => {
+        console.log('按钮被点击了4', name, e)
+    }
+
     return (
         <div>
-            {/* 通过调用函数来渲染 */}
-            {renderContent()} <br />
+            {/* 基础绑定 */}
+            <button onClick={handleClick01}> 基础绑定测试 </button>
+
+            {/* 事件参数绑定 */}
+            <button onClick={(e) => handleClick02(e)}> 事件参数绑定测试 </button>
+
+            {/* 自定义参数 绑定 */}
+            <button onClick={() => handleClick03('jack')}> 自定义参数绑定测试 </button>
+
+            {/* 自定义参数 + 事件参数 绑定 */}
+            <button onClick={(e) => handleClick04('mary', e)}> 自定义参数 + 事件参数绑定测试 </button>
+
         </div>
     );
 }
