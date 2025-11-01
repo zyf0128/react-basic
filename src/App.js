555ca505b00e694adb6d1417dc66e2e85220ae13
zhuyin <zhuyin_dsw@163.com>
2025-11-01 10:22:42 +0800

    useState的使用,实现一个简单的技术器

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-// 组件的定义和使用
+// useState 是 React 中用于在函数组件中添加状态管理的 Hook。
+// 目标： 使用useState实现一个简单的计数器功能
 
-// 定义组件
-function Button() {
-    // 实现业务逻辑
-    return <button>Click Me</button>;
-}
+// 从react 中导入 useState 函数
+import { useState } from 'react';
+
 
 
 // 项目的根组件
 function App() {
 
+    // 调用useState 来 添加一个 状态变量 
+    // useState返回值是一个数组
+    // count 是状态变量
+    // setCount 是用来修改状态变量的方法
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        /* 
+        setCount作用：
+         1. 修改count 的值
+         2. 使用新值来进行UI的渲染
+        */
+        setCount(count + 1)
+    }
+
     return (
         <div>
-            {/* 自闭合标签 */}
-            <Button />
-
-            {/* 双标签 */}
-            <Button></Button>
+            {/* 这里进行事件的绑定 */}
+            <button onClick={handleClick}>{count}</button>
         </div>
     );
 }
